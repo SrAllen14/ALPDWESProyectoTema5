24559bbbf1b2ff1e4cbd88645ac54df31a9719b9
Alvaro <alvaro.allper.1@educa.jcyl.es>
2025-11-19 19:00:34 +0100

    Acabado ejercicio2.php Creado ejercicio2_1.php (formulario artesano)

--- codigoPHP/ejercicio2.php
+++ codigoPHP/ejercicio2.php
@@ -69,90 +69,49 @@
                 require_once '../core/miLibreriaValidacion.php';
                 require_once '../config/confDB.php';
 
-                $nombreUsuario = "alvaro";
-                $contrasenaUsuario = hash('sha256', 'paso');
-
-                //inicialización de variables
-                $aErrores = [
-                    'user' => '',
-                    'pass' => '',
-                ];
-                $aRespuestas = [
-                    'user' => '',
-                    'pass' => '',
-                ];
-                $entradaOK = true;
-
-                //Para cada campo del formulario se valida la entrada y se actua en consecuencia
-                if (isset($_REQUEST['enviar'])) {//se cumple si el boton es submit
-                    //Validación de los datos de los campos del formulario
-                    $aErrores['user'] = validacionFormularios::comprobarAlfanumerico($_REQUEST['user'], 80, 1, 1);
-                    $aErrores['pass'] = validacionFormularios::comprobarAlfanumerico($_REQUEST['pass'], 8, 4, 1);
-
-                    //recorre el array de errores para detectar si hay alguno
-                    foreach ($aErrores as $campo => $valorCampo) {
-                        if ($valorCampo != null) {//Si encuentra algún error 
-                            $entradaOK = false; // la entrada no es correcta
-                        }
-                    }
-                } else {
-                    //Si no se ha aceptado el formulario
-                    $entradaOK = false;
-                }
-                //Tratamiento del formulario
-                if ($entradaOK) {
-                    //Rellenamos el array de respuesta con los valores que ha introducido el usuario
-                    $aRespuestas['user'] = $_REQUEST['user'];
-                    $aRespuestas['pass'] = $_REQUEST['pass'];
+                $validacionUsuario = false;
+                
+                if(!isset($_SERVER['PHP_AUTH_USER'])){
+                    header('WWW-Authenticate: Basic realm="Contenido restringido"');
+                    header('HTTP/1.0 401 Unauthorized');
                     
+                    echo '<h3>Acceso restringido</h3>';
+                    exit;
+                } else{
                     try{
+                        $usuarioPassword = $_SERVER['PHP_AUTH_USER'].$_SERVER['PHP_AUTH_PW'];
+                        
                         $miDB = new PDO(DSN, USERNAME, PASSWORD);
                         
-                        $sql = "SELECT T01_CodUsuario, T01_Password FROM T01_Usuario WHERE T01_CodUsuario = :usuario";
-                        $consultaPreparada = $miDB->prepare($sql);
+                        $sql = "SELECT * FROM T01_Usuario WHERE T01_CodUsuario = :usuario AND T01_Password = sha2(:pass, 256)";
+                        $consulta = $miDB->prepare($sql);
+                        $consulta->execute([
+                            ':usuario' => $_SERVER['PHP_AUTH_USER'],
+                            ':pass' => $usuarioPassword
+                        ]);
                         
-                        $consultaPreparada->bindParam(':usuario', $aRespuestas['user']);
-                        $consultaPreparada->execute();
-                        
-                        $aResultados = $consultaPreparada->fetch();
-                        
-                        if ($aRespuestas["user"] === $aResultados['T01_CodUsuario']) {
-                            if (hash('sha256', $aRespuestas["user"]."".$aRespuestas["pass"]) === $aResultados['T01_Password']) {
-                                // En caso de que la contraseña sea correcta saludamos al usuario.
-                                print('<br><h3>Bienvenido: ' . $aRespuestas["user"] . '</h3><br>');
-                            } else {
-                                // En caso de que la contraseña sea incorrecta se lo indicamos al usuario.
-                                print('<br><h3>Contraseña incorrecta</h3>');
-                            }
-                        } else {
-                            // En caso de que el usuario no exista lo indicamos.
-                            print('<h3>El usuario no existe</h3>');
+                        $resultado = $consulta->rowCount();
+                        if($resultado === 1){
+                            $validacionUsuario = true;
                         }
                     } catch(PDOException $exPDO){
-                        echo 'Error al conectarse: '.$exPDO->getMessage();
-                        echo 'Codigo de error: '.$exPDO->getCode();
-                        echo 'Linea de error: '.$exPDO->getLine();
+                        echo '<p>Mensaje de error: '.$exPDO->getMessage().'</p>';
+                    } finally {
+                        unset($miDB);
                     }
-                } else {
-                    //si hay algún error se vuelve a mostrar el formulario
-                    ?>
-                    <section>
-                        <h2>Rellena el formulario.</h2>
-                        <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post">
-                            <label for="user">Usuario:</label>
-                            <a style='color:red'><?php echo $aErrores['user'] ?></a><br>
-                            <input name="user" id="user" type="text" value='<?php echo(empty($aErrores['user'])) ? ($_REQUEST['user'] ?? '') : ''; ?>'><br>
-                            <br>
-                            <label for="pass">Contraseña:</label>
-                            <a style='color:red'><?php echo $aErrores['pass'] ?></a><br>
-                            <input name="pass" id="pass" type="text" value='<?php echo(empty($aErrores['pass'])) ? ($_REQUEST['pass'] ?? '') : ''; ?>'><br>
-                            <br>
-                            <button type="submit" name="enviar">Enviar</button>
-
-                        </form>  
-                        <?php
+                    
+                    if(!$validacionUsuario){
+                        header('WWW-Authenticate: Basic realm="Contenido restringido"');
+                        header('HTTP/1.0 401 Unauthorized');
+                    
+                        echo '<h3>Acceso restringido</h3>';
+                        exit;
                     }
-                    ?>
+                }
+                ?>
+                
+                <h3>Acceso permitido</h3>
+                <p>Bienvenido <strong><?php echo $_SERVER['PHP_AUTH_USER']?></strong></p>
             </div>
         </main>
         <footer>
@@ -160,7 +119,7 @@
                 <a href="../indexProyectoTema5.php">
                     Álvaro Allén Perlines
                 </a>
-                <time datetime="2025-11-17">17-11-2025</time>
+                <time datetime="2025-11-19">19-11-2025</time>
             </div>
         </footer>
     </body>
